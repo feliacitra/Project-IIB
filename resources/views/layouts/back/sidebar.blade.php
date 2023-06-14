57c3ce9e4cf6547acffcbdb52feb3234dbf10378
feliacitra <feliacitra28@gmail.com>
2023-06-14 15:54:04 +0700

    First Upload

---
+++ resources/views/layouts/back/sidebar.blade.php
@@ -0,0 +1,23 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar">
+    <div class="sidebar-header">
+        <img class="sidebar-brand-image" src="{{ asset('back/images/logo/main_logo.png') }}" alt="main_logo" width="45%">
+        <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="sidebar-body">
+        <ul class="nav">
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="link-icon" data-feather="box"></i>
+                    <span class="link-title">Dashboard</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- partial -->
