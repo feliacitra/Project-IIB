0170a69f6f2cbd466dd2678e8cfc4a42b847c3c5
Padliwinata <rpadliwinata@gmail.com>
2023-07-05 01:38:06 +0700

    fix: master user bugs

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected $attributes = [
+        'role' => '2',
+    ];
+
     /**
      * Get the user_detail associated with the User
      *
