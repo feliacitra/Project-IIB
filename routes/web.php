2e5719e5494e327c0953e3d1a2f34656e31a052f
Kurnia N. A. Dalimunthe <106486323+Nia-Dalimunthe@users.noreply.github.com>
2023-07-28 11:53:08 +0700

    Dashboard User

--- routes/web.php
+++ routes/web.php
@@ -115,6 +115,10 @@ Route::middleware(['auth', 'access'])->group(function () {
         return view('Pendaftaran.dataPendaftar');
     })->name('pendaftar');
 
+    Route::get('/daftar', function() {
+        return view('Startup.daftarStartup');
+    })->name('daftar');
+
     // Route::get('/master/inkubasi', function() {
     //     $master_programinkubasi = DB::table('master_programinkubasi')->get();
     //     return view('Master-ProgramInkubasi.listProgramInkubasi',['master_programinkubasi'=>$master_programinkubasi]);
