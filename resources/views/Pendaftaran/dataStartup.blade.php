d5a3b8686e5d4587e06707fd1231030575a9e5e6
Kurnia N. A. Dalimunthe <106486323+Nia-Dalimunthe@users.noreply.github.com>
2023-08-02 10:23:10 +0700

    Fix view data startup

--- resources/views/Pendaftaran/dataStartup.blade.php
+++ resources/views/Pendaftaran/dataStartup.blade.php
@@ -237,4 +237,18 @@
             {{-- Self Assessment --}}
         </div>
     </div>
+
+    <script>
+        $('.btnNext').click(function() {
+            const nextTabLinkEl = $('.nav-tabs .active').closest('li').next('li').find('a')[0];
+            const nextTab = new bootstrap.Tab(nextTabLinkEl);
+            nextTab.show();
+        });
+
+        $('.btnPrevious').click(function() {
+            const prevTabLinkEl = $('.nav-tabs .active').closest('li').prev('li').find('a')[0];
+            const prevTab = new bootstrap.Tab(prevTabLinkEl);
+            prevTab.show();
+        });
+    </script>
 @endsection
